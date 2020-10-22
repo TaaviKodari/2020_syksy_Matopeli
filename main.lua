5f761ed52e3a7054675c886885cf066edaf6cb35
kodarit11 <taavi.makela@gmail.com>
2020-10-22 18:17:59 +0300

    ruokkiminen ja syöminen tehty

--- main.lua
+++ main.lua
@@ -16,8 +16,22 @@ function reset()
 	
 	suunta = 'right'
 	ajastin = 0
+	aikaraja  =  0.15
+	ruoki()
+end
+
+function ruoki()
+	ruoka = {}
+	
+	ruokaX  = love.math.random(1,leveys)
+	ruokaY = love.math.random(1,korkeus)
+	
+	ruoka.x  =   ruokaX
+	ruoka.y  =  ruokaY
+	-- pisteen lisäys
 	
 end
+
 --funktio peliframien piirtämiseen
 function love.draw()
 	
@@ -30,6 +44,10 @@ function love.draw()
 	local function piirraRuutu(x,y)
 		love.graphics.rectangle('fill', (x-1) * ruutu, (y-1) * ruutu, ruutu-1 ,ruutu-1)
 	end
+	
+	love.graphics.setColor(1,.3,.3)
+	piirraRuutu(ruoka.x,ruoka.y)
+	
 	-- kommentointi
 	for indeksi , matopala in ipairs(mato) do
 		love.graphics.setColor(0.6, 0.9, 0.32)
@@ -40,28 +58,89 @@ end
 
 function love.update(dt)
 
-	local seuraavaX = mato[1].x
-	local seuraavaY = mato[1].y
+	ajastin = ajastin + dt
 	
-	if suunta == 'right' then
-		seuraavaX = seuraavaX + 1
+	if ajastin >= aikaraja then 
 		
-		if  seuraavaX  > leveys then
-			suunta = 'left'
+		ajastin = ajastin - aikaraja
+		
+		local seuraavaX = mato[1].x
+		local seuraavaY = mato[1].y
+		
+		peliJatkuu =  true
+		
+		if suunta == 'right' then
+			seuraavaX = seuraavaX + 1
+		
+			if  seuraavaX  > leveys  then
+				--gameover
+				peliJatkuu = false
+			end
+		
+		elseif suunta == 'left' then
+			seuraavaX = seuraavaX - 1
+		
+			if seuraavaX  < 1 then
+				--gameover
+				peliJatkuu = false
+			end
+		
+		elseif suunta == 'down' then
+			seuraavaY = seuraavaY + 1
+			
+			if seuraavaY  >  korkeus  then
+				--gameover
+				peliJatkuu = false
+			end
+		
+		elseif suunta == 'up' then
+			seuraavaY  = seuraavaY - 1
+			
+			if seuraavaY   < 1 then
+				--gameover
+				peliJatkuu = false
+			end
 		end
 		
-	elseif suunta == 'left' then
-		seuraavaX = seuraavaX - 1
-		
-		if seuraavaX  < 1 then
-			suunta = 'right'
+		if peliJatkuu then
+			table.insert(mato,1,{x = seuraavaX, y = seuraavaY})
+			
+			if mato[1].x == ruoka.x and mato[1].y == ruoka.y then 
+				ruoki()
+
+				if aikaraja >  0.2 then 
+					aikaraja = aikaraja - 0.1
+				end
+			else
+			table.remove(mato)			
+			end
 		end
+
+	end
+end
+
+function  love.keypressed(key)
+	
+	if key == 'right' and  peliJatkuu  == true  and  suunta ~= 'left'  then
+		
+		suunta = 'right'
+		
+	elseif  key == 'left' and peliJatkuu and suunta ~= 'right' then
+		
+		suunta = 'left'
+		
+	elseif key == 'up' and  peliJatkuu and suunta ~= 'down' then
+		
+		suunta = 'up'
+		
+	elseif key == 'down' and peliJatkuu and suunta ~='up' then
+		
+		suunta = 'down'
+	
 	end
 	
-	table.insert(mato,1,{x = seuraavaX, y = seuraavaY})
-	table.remove(mato)
-	
 end
+
 
 
 
